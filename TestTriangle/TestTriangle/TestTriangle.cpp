188d8650966b1f0f13120bba4575a360c17d8c08
wjingzhe <455786463@qq.com>
2018-08-02 00:12:43 +0800

    GUI/Position

--- TestTriangle/TestTriangle/TestTriangle.cpp
+++ TestTriangle/TestTriangle/TestTriangle.cpp
@@ -11,8 +11,11 @@
 #include "../../DXUT/Core/DDSTextureLoader.h"
 #include "../../DXUT/Core/WICTextureLoader.h"
 #include <algorithm>
+#include "../../DXUT/Optional/DXUTSettingsDlg.h"
 
 using namespace DirectX;
+
+static const int TEXT_LINE_HEIGHT = 15;
 
 #define TRIANGLE
 #define DECAL
@@ -45,8 +48,24 @@ ID3D11ShaderResourceView* g_pTempDepthStencilSRV = nullptr;
 ID3D11Texture2D* g_pDecalTexture = nullptr;
 ID3D11ShaderResourceView* g_pDecalTextureSRV = nullptr;
 static float g_fMaxDistance = 1000.0f;
-//CFirstPersonCamera g_Camera;
-CModelViewerCamera g_Camera;
+CFirstPersonCamera g_Camera;
+//CModelViewerCamera g_Camera;
+
+static AMD::HUD             g_HUD;
+static CDXUTDialogResourceManager  g_DialogResourceManager; // manager for shared resources of dialogs
+static CDXUTTextHelper*			g_pTextHelper = nullptr;
+CD3DSettingsDlg             g_D3dSettingsGUI;           // Device settings dialog
+
+//GUI
+static bool g_bRenderHUD = false;
+static bool g_bCD3DSettingsDlgActive = false;
+
+enum
+{
+	IDC_BUTTON_TOGGLEFULLSCREEN = 1,
+	IDC_BUTTON_CHANGEDEVICE,
+	IDC_NUM_CONTROL_IDS
+};
 
 // Depth stencil states
 ID3D11DepthStencilState* g_pDepthStencilDefaultDS = nullptr;
@@ -77,7 +96,12 @@ void CALLBACK OnD3D11DestroyDevice(void* pUserContext);
 void CALLBACK OnFrameRender(ID3D11Device* pD3dDevice, ID3D11DeviceContext* pD3dImmediateContext,
 	double fTime, float fElapsedTime, void* pUserContext);
 
+void RenderText();
+
 void InitApp();
+
+void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext);
+void CALLBACK OnGUIEvent(UINT nEvent, int nControlID, CDXUTControl* pControl, void* pUserContext);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
                      _In_opt_ HINSTANCE hPrevInstance,
@@ -87,11 +111,17 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
 
+	// Enable run-time memory check for debug builds.
+#if defined(DEBUG) || defined(_DEBUG)
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
     // TODO: Place code here.
 
 	// Set DXUT callbacks
 
 	DXUTSetCallbackMsgProc(MsgProc);
+	DXUTSetCallbackKeyboard(OnKeyboard);
 	DXUTSetCallbackDeviceChanging(ModifyDeviceSettings);
 	DXUTSetCallbackFrameMove(OnFrameTimeUpdated);
 
@@ -142,6 +172,24 @@ LRESULT  CALLBACK MsgProc(HWND hWnd, UINT uMsg,WPARAM wParam, LPARAM lParam, boo
 	{
 		return 0;
 	}
+
+	// Pass messages to dialog resource manager calls so GUI state is updated correctly
+	*pbNoFurtherProcessing = g_DialogResourceManager.MsgProc(hWnd, uMsg, wParam, lParam);
+	if (*pbNoFurtherProcessing)
+		return 0;
+
+	// Pass messages to settings dialog if its active
+	if (g_D3dSettingsGUI.IsActive())
+	{
+		g_D3dSettingsGUI.MsgProc(hWnd, uMsg, wParam, lParam);
+		return 0;
+	}
+
+	// Give the dialogs a chance to handle the message first
+	*pbNoFurtherProcessing = g_HUD.m_GUI.MsgProc(hWnd, uMsg, wParam, lParam);
+	if (*pbNoFurtherProcessing)
+		return 0;
+
 
 	// Pass all remaining windows message to camera ,sot it can respond to user input
 	// 常规按键处理
@@ -237,15 +285,18 @@ HRESULT CALLBACK OnD3D11DeviceCreated(ID3D11Device * pD3dDevice, const DXGI_SURF
 	
 
 	//load the scene mesh
-
+	XMVECTOR pos4 = XMVectorSet(0.0f, -100.0f, 500.0f, 1.0f);
 #ifdef TRIANGLE
-	s_TriangleRender.GenerateMeshData(100, 100, 100, 6, 0.0f, -100.0f, 500.0f);
+	s_TriangleRender.GenerateMeshData(100, 100, 100, 6);
+	s_TriangleRender.SetPosition(pos4);
 	Triangle::TriangleRender::CalculateSceneMinMax(s_TriangleRender.m_MeshData, &SceneMin, &SceneMax);
+	SceneMin += pos4;
+	SceneMax += pos4;
 	V_RETURN(s_TriangleRender.OnD3DDeviceCreated(pD3dDevice, pBackBufferSurfaceDesc, pUserContext));
 #endif
 
 #ifdef DECAL
-	s_DeferredDecalRender.GenerateMeshData(100, 100, 100, 6);
+	s_DeferredDecalRender.GenerateMeshData(101, 101, 101, 6);
 	//PostProcess::DeferredDecalRender::CalculateSceneMinMax(s_DeferredDecalRender.m_MeshData, &SceneMin, &SceneMax);
 	V_RETURN(s_DeferredDecalRender.OnD3DDeviceCreated(pD3dDevice, pBackBufferSurfaceDesc, pUserContext));
 	s_DeferredDecalRender.SetDecalTextureSRV(g_pDecalTextureSRV);
@@ -253,6 +304,9 @@ HRESULT CALLBACK OnD3D11DeviceCreated(ID3D11Device * pD3dDevice, const DXGI_SURF
 
 	static bool bFirstPass = true;
 
+	//create AMD_SDK resource here
+	TIMER_Init(pD3dDevice);
+	g_HUD.OnCreateDevice(pD3dDevice);
 
 	// One-time setup
 	if (bFirstPass)
@@ -266,8 +320,8 @@ HRESULT CALLBACK OnD3D11DeviceCreated(ID3D11Device * pD3dDevice, const DXGI_SURF
 
 
 		XMVECTOR vEye = XMVectorSet(0.0, 0.0f, 0.0f,0.0f);
-		XMVECTOR vLookAtPos = SceneCenter - XMVectorSet(0.0f,0.0f, -10.0f, 0.0f);//要看到前面的剑法
-		g_Camera.SetButtonMasks();//left_button can rotate camera
+		XMVECTOR vLookAtPos = SceneCenter + XMVectorSet(0.0f,0.0f, 10.0f, 0.0f);//要看到前面的剑法
+		g_Camera.SetRotateButtons(true, false, false);
 		g_Camera.SetEnablePositionMovement(true);
 		g_Camera.SetViewParams(vEye, vLookAtPos);
 		g_Camera.SetScalers(0.005f, 0.1f*g_fMaxDistance);
@@ -276,8 +330,11 @@ HRESULT CALLBACK OnD3D11DeviceCreated(ID3D11Device * pD3dDevice, const DXGI_SURF
 		XMStoreFloat3(&vBoundaryMin, BoundaryMin);
 		XMStoreFloat3(&vBoundaryMax, BoundaryMax);
 		g_Camera.SetClipToBoundary(true, &vBoundaryMin, &vBoundaryMax);
+
+
+
 #ifdef DECAL
-		s_DeferredDecalRender.SetDecalPosition(SceneCenter - XMVectorSet(0.0f, 50.0f, 0.0f, 0.0f));
+		s_DeferredDecalRender.SetPosition(SceneCenter);
 #endif // DECAL
 
 
@@ -294,14 +351,21 @@ HRESULT CALLBACK OnD3D11DeviceCreated(ID3D11Device * pD3dDevice, const DXGI_SURF
 		bFirstPass = false;
 	}
 
+
+
+	V_RETURN(g_DialogResourceManager.OnD3D11CreateDevice(pD3dDevice, pD3dImmediateContext));
+	V_RETURN(g_D3dSettingsGUI.OnD3D11CreateDevice(pD3dDevice));
+	SAFE_DELETE(g_pTextHelper);
+	g_pTextHelper = new CDXUTTextHelper(pD3dDevice, pD3dImmediateContext, &g_DialogResourceManager, TEXT_LINE_HEIGHT);
+
 	pD3dImmediateContext = nullptr;
 
-	return S_OK;
+	return hr;
 }
 
 HRESULT AddShadersToCache()
 {
-	HRESULT hr = E_FAIL;
+	HRESULT hr = S_OK;
 
 #ifdef TRIANGLE
 	s_TriangleRender.AddShadersToCache(&g_ShaderCache);
@@ -317,6 +381,15 @@ HRESULT AddShadersToCache()
 HRESULT OnD3D11ResizedSwapChain(ID3D11Device * pD3dDevice, IDXGISwapChain * pSwapChain, const DXGI_SURFACE_DESC * pBackBufferSurfaceDesc, void * pUserContext)
 {
 	HRESULT hr;
+
+	V_RETURN(g_DialogResourceManager.OnD3D11ResizedSwapChain(pD3dDevice, pBackBufferSurfaceDesc));
+	V_RETURN(g_D3dSettingsGUI.OnD3D11ResizedSwapChain(pD3dDevice, pBackBufferSurfaceDesc));
+
+	//Set the location and size of AMD standard HUD
+	g_HUD.m_GUI.SetLocation(pBackBufferSurfaceDesc->Width - AMD::HUD::iDialogWidth, 0);
+	g_HUD.m_GUI.SetSize(AMD::HUD::iDialogWidth, pBackBufferSurfaceDesc->Height);
+	g_HUD.OnResizedSwapChain(pBackBufferSurfaceDesc);
+
 
 	// Setup the camera's projection parameter
 	float fApsectRatio = pBackBufferSurfaceDesc->Width / (FLOAT)pBackBufferSurfaceDesc->Height;
@@ -345,9 +418,7 @@ HRESULT OnD3D11ResizedSwapChain(ID3D11Device * pD3dDevice, IDXGISwapChain * pSwa
 	s_DeferredDecalRender.OnResizedSwapChain(pD3dDevice, pBackBufferSurfaceDesc);
 #endif
 
-
-
-	return S_OK;
+	return hr;
 }
 
 void OnD3D11ReleasingSwapChain(void * pUserContext)
@@ -374,8 +445,11 @@ void OnD3D11ReleasingSwapChain(void * pUserContext)
 
 void OnD3D11DestroyDevice(void * pUserContext)
 {
+	g_DialogResourceManager.OnD3D11DestroyDevice();
+	g_D3dSettingsGUI.OnD3D11DestroyDevice();
+	SAFE_DELETE(g_pTextHelper);
 	DXUTGetGlobalResourceCache().OnDestroyDevice();
-	g_ShaderCache.OnDestroyDevice();
+
 
 	SAFE_RELEASE(g_pDepthStencilDefaultDS);
 
@@ -397,7 +471,11 @@ void OnD3D11DestroyDevice(void * pUserContext)
 	s_DeferredDecalRender.OnD3D11DestroyDevice(pUserContext);
 #endif
 	
-	
+	//AMD
+	g_ShaderCache.OnDestroyDevice();
+	g_HUD.OnDestroyDevice();
+	TIMER_Destroy();
+
 }
 
 
@@ -476,6 +554,17 @@ void ClearD3D11DeviceContext()
 void OnFrameRender(ID3D11Device * pD3dDevice, ID3D11DeviceContext * pD3dImmediateContext, double fTime, float fElapsedTime, void* pUserContext)
 {
 
+	// Reset the timer at start of frame
+	TIMER_Reset();
+	
+	// If the settings dialog is being shown, then render it instead of rendering the app's scene
+	if (g_D3dSettingsGUI.IsActive())
+	{
+		g_D3dSettingsGUI.OnRender(fElapsedTime);
+		return;
+	}
+
+
 	ClearD3D11DeviceContext();
 
 	const DXGI_SURFACE_DESC* pBackBufferDesc = DXUTGetDXGIBackBufferSurfaceDesc();
@@ -487,15 +576,14 @@ void OnFrameRender(ID3D11Device * pD3dDevice, ID3D11DeviceContext * pD3dImmediat
 	pD3dImmediateContext->ClearRenderTargetView(pRTV, ClearColor);
 	pD3dImmediateContext->ClearDepthStencilView(g_pDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 
-
-
-
-
+	// Switch off alpha blending
+	// float BlendFactor[4] = { 1.0f,1.0f,0.0f,0.0f }; 这些参数没什么用
+	pD3dImmediateContext->OMSetBlendState(nullptr, nullptr, 0xFFFFFFFF);
 
 	//Render or objects here..
-	if (g_ShaderCache.ShadersReady(true))
+	if (g_ShaderCache.ShadersReady(false))
 	{
-
+		TIMER_Begin(0, L"Render");
 #ifdef TRIANGLE
 		pD3dImmediateContext->RSSetState(nullptr);
 		pD3dImmediateContext->OMSetDepthStencilState(g_pDepthStencilDefaultDS, 0x00);
@@ -510,19 +598,123 @@ void OnFrameRender(ID3D11Device * pD3dDevice, ID3D11DeviceContext * pD3dImmediat
 		s_DeferredDecalRender.OnRender(pD3dDevice, pD3dImmediateContext, pBackBufferDesc, &g_Camera, pRTV, g_pTempDepthStencilView, g_pDepthStencilSRV);
 #endif
 
-	
+		TIMER_End(); // Render
+
+
+		RenderText();
+
+
+		if (g_bRenderHUD)
+		{
+			g_HUD.OnRender(fElapsedTime);
+		}
+
 	}
 	else
 	{
 		// Render shader cache progress if still processing
 		pD3dImmediateContext->OMSetRenderTargets(1, (ID3D11RenderTargetView*const*)&pRTV, g_pDepthStencilView);
-		//g_ShaderCache.RenderProgress(g_pTextHelper, 15, XMVectorSet(1.0f, 1.0f, 0.0f, 1.0f));
+		g_ShaderCache.RenderProgress(g_pTextHelper, 15, XMVectorSet(1.0f, 1.0f, 0.0f, 1.0f));
 
 		Sleep(1);
 	}
 
 }
 
+void RenderText()
+{
+	g_pTextHelper->Begin();
+	g_pTextHelper->SetInsertionPos(5, 5);
+	g_pTextHelper->SetForegroundColor(XMVectorSet(0.0f, 1.0f, 0.0f, 1.0f));
+	g_pTextHelper->DrawTextLine(DXUTGetFrameStats(DXUTIsVsyncEnabled()));
+	g_pTextHelper->DrawTextLine(DXUTGetDeviceStats());
+
+	const float fGpuTime = (float)TIMER_GetTime(Gpu, L"Render")*1000.0f;
+
+	// count digits in the total time;
+	int iIntergetPart = (int)fGpuTime;
+	int iNumDigits = 0;
+	while (iIntergetPart > 0)
+	{
+		iIntergetPart /= 10;
+		iNumDigits++;
+	}
+	iNumDigits = (iNumDigits == 0) ? 1 : iNumDigits;
+	//three digits after decimal,
+	// plus the decimal point itself
+	int iNumChars = iNumDigits + 4;
+
+	//dynamic formating for sprintf_s
+	WCHAR szPrecision[16];
+	swprintf_s(szPrecision, 16, L"%%%d.3f", iNumChars);
+
+	WCHAR szBuf[256];
+	WCHAR szFormat[256];
+	swprintf_s(szFormat, 256, L"Total:        %s", szPrecision);
+	swprintf_s(szBuf, 256, szFormat, fGpuTime);
+	g_pTextHelper->DrawTextLine(szBuf);
+
+
+
+	g_pTextHelper->SetInsertionPos(5, DXUTGetDXGIBackBufferSurfaceDesc()->Height - AMD::HUD::iElementDelta);
+	g_pTextHelper->DrawTextLine(L"Toggle GUI    : F1");
+
+	g_pTextHelper->End();
+}
+
 void InitApp()
 {
+	g_D3dSettingsGUI.Init(&g_DialogResourceManager);
+
+
+	WCHAR szTemp[256];
+
+	D3DCOLOR DlgColor = 0x88888888;//Semi-transparent background for the dialog
+
+	auto& GUI = g_HUD.m_GUI;
+
+	GUI.Init(&g_DialogResourceManager);
+	GUI.SetBackgroundColors(DlgColor);
+	GUI.SetCallback(OnGUIEvent);
+
+	int iY = AMD::HUD::iElementDelta;
+
+	GUI.AddButton(IDC_BUTTON_TOGGLEFULLSCREEN, L"Toggle full screen", AMD::HUD::iElementOffset, iY, AMD::HUD::iElementWidth, AMD::HUD::iElementHeight, VK_F11);
+	GUI.AddButton(IDC_BUTTON_CHANGEDEVICE, L"Change device (F2)", AMD::HUD::iElementOffset, iY += AMD::HUD::iElementDelta, AMD::HUD::iElementWidth, AMD::HUD::iElementHeight, VK_F2);
+
+}
+
+
+//--------------------------------------------------------------------------------------
+// Handle key presses
+//--------------------------------------------------------------------------------------
+void CALLBACK OnKeyboard(UINT nChar, bool bKeyDown, bool bAltDown, void* pUserContext)
+{
+	if (bKeyDown)
+	{
+		switch (nChar)
+		{
+		case VK_F1:
+			g_bRenderHUD = !g_bRenderHUD;
+			break;
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------
+// Handles the GUI events
+//--------------------------------------------------------------------------------------
+void CALLBACK OnGUIEvent(UINT nEvent, int nControlID, CDXUTControl * pControl, void * pUserContext)
+{
+	WCHAR szTemp[256];
+
+	switch (nControlID)
+	{
+	case IDC_BUTTON_TOGGLEFULLSCREEN:
+		DXUTToggleFullScreen();
+		break;
+	case IDC_BUTTON_CHANGEDEVICE:
+		g_D3dSettingsGUI.SetActive(!g_D3dSettingsGUI.IsActive());
+		break;
+	}
 }
